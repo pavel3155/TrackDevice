7f22f857ca3ee350ef35de8294596d6a7e6f6658
Koval <pavel1381@list.ru>
2025-07-31 18:10:35 +0500

    310725

--- src/main/java/com/example/TrackDevice/controllers/DevicesController.java
+++ src/main/java/com/example/TrackDevice/controllers/DevicesController.java
@@ -1,29 +1,23 @@
 package com.example.TrackDevice.controllers;
 
 import com.example.TrackDevice.DTO.DeviceDTO;
-import com.example.TrackDevice.DTO.OrdersDTO;
 import com.example.TrackDevice.Poiji.PoijiDevice;
 import com.example.TrackDevice.model.CSA;
 import com.example.TrackDevice.model.ModelDevice;
-import com.example.TrackDevice.model.Order;
 import com.example.TrackDevice.repo.CSARepository;
 import com.example.TrackDevice.repo.ModelDeviceRepository;
 import com.example.TrackDevice.service.DeviceService;
 import com.example.TrackDevice.service.FileService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/Devices")
@@ -61,6 +55,8 @@ public class DevicesController {
         System.out.println("deviceDTO:="+deviceDTO);
 
         model.addAttribute("deviceDTO",deviceDTO);
+        model.addAttribute("import",false);
+
         return "Devices/imp_devices";
     }
     @PostMapping("/imp_devices")
@@ -70,21 +66,64 @@ public class DevicesController {
         System.out.println("deviceDTO:= "+deviceDTO);
 
         MultipartFile multipartFile = multipartFiles[0];
+        int numberEntries=0;
+        int impNumberRecords=0;
+        int notImpNumberRecords=0;
+        boolean success=true;
 
         if (fileService.saveFile(multipartFile,"import","devices")){
             String locationFile=fileService.getPathFile(multipartFile,"import","devices");
             List<PoijiDevice> lstDevices= fileService.getListDevicesFromFileExcel(locationFile).stream()
                     .filter(poijiDevice -> poijiDevice.getModel().equals(deviceDTO.getModelDevice().getName()))
                             .toList();
-            System.out.println("lstDevices:= "+lstDevices);
+
+            numberEntries=lstDevices.size();
+
             System.out.println("кол-во ТС отобранных для загрузки:= "+lstDevices.size());
             deviceDTO.setStatus("исправный");
             for(PoijiDevice poijiDevice : lstDevices){
                 deviceDTO.setInvnum(poijiDevice.getInvNum());
                 deviceDTO.setSernum(poijiDevice.getSerNum());
-                deviceService.addDevice(deviceDTO);
+
+                try {
+                    deviceService.addDevice(deviceDTO);
+                    poijiDevice.setSuccess(true);
+                    impNumberRecords=impNumberRecords+1;
+                } catch (DataIntegrityViolationException e) {
+                    poijiDevice.setSuccess(false);
+                    poijiDevice.setComment("серийный(инвентарный) номер уже существует");
+                    notImpNumberRecords=notImpNumberRecords+1;
+                    success=false;
+
+//                    System.out.println("e.getMessage()={***"+e.getMessage()+"***}");
+//                    System.out.println("e.toString()={***"+e.toString()+"***}");
+//                    System.out.println("e.getCause()={***"+e.getCause()+"***}");
+//                    System.out.println("e.getRootCause()={***"+e.getRootCause()+"***}");
+//                    System.out.println("e.getLocalizedMessage()={***"+e.getLocalizedMessage()+"***}");
+//                    System.out.println("e.getMostSpecificCause()={***"+e.getMostSpecificCause()+"***}");
+
+                } catch (Exception ex) {
+                    success=false;
+                }
+
             }
+
+            List<PoijiDevice> notImpDev= lstDevices.stream()
+                    .filter(poijiDevice -> !(poijiDevice.getSuccess()))
+                    .toList();
+            model.addAttribute("notImpDev",notImpDev);
         }
+
+        model.addAttribute("numberEntries",numberEntries);
+        model.addAttribute("impNumberRecords",impNumberRecords);
+        model.addAttribute("notImpNumberRecords",notImpNumberRecords);
+        model.addAttribute("success", success);
+        model.addAttribute("import",true);
+
+
+
+
+
         return "Devices/imp_devices";
 
 
